15e7bb95a8fbbf5cb00ee1600b0f99e4071d71f2
SRIRANJANI <152777504+SRIRANJANI-SEKARAN@users.noreply.github.com>
2023-12-03 18:27:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,68 @@
+/* --------------------------------------------------------------Excercise 1-------------------------------------------------------------------
+
+1. Write a single line comment which says, comments can make code readable
+
+2. Write another single comment which says, Welcome to 30DaysOfJavaScript
+
+3. Write a multiline comment which says, comments can make code readable, easy to reuse and informative
+
+4. Create a variable.js file and declare variables and assign string, boolean, undefined and null data types
+
+5. Create datatypes.js file and use the JavaScript typeof operator to check different data types. Check the data type of each variable
+
+6. Declare four variables without assigning values
+
+7. Declare four variables with assigned values
+
+8. Declare variables to store your first name, last name, marital status, country and age in multiple lines
+
+9. Declare variables to store your first name, last name, marital status, country and age in a single line
+
+10. Declare two variables myAge and yourAge and assign them initial values and log to the browser console.
+ 
+-----------------------------------------------------------------------------------------------------------------------------------------------
+*/
+
+
+//---------------------------------------------------------------SOLUTIONS---------------------------------------------------------------------
+//1. comments can make code readable
+//2. Welcome to 30DysOfJavaScript
+/*3. comments can make code readable, easy to reuse and informative
+ */
+
+//4
+let var1='Ranjani';
+let var2=true;
+let var3;
+let var4=null;
+
+//5
+ console.log(typeof var1);
+ console.log(typeof var2);
+ console.log(typeof var3);
+ console.log(typeof var4);
+//6
+ let fName,lName,age,sex; 
+
+ //7
+ let fstName='Ranjani';
+ let lstName='c';
+ let agee=21;
+ let sexx='Female'; 
+
+//8
+let firstName='Ranajni';
+let lastName='c';
+let isMarried=false;
+let MycountryName='India';
+let Myage= 22;
+
+//9
+let fName='Ranajni',lName='c',isMarried=false,countryName='India',age= 22;
+
+//10
+let myAge=22;
+let yourAge=24;
+console.log(`I am ${myAge} years old
+You are ${yourAge} years old.`);
+ 
